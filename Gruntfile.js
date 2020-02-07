b24fea89410cf75750554408c0301b7a82ec11f4
juang0617 <47310589+juang0617@users.noreply.github.com>
2020-02-06 19:43:21 -0500

    Instalación cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,24 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'js/main.js',
+        dest: 'js/main.min.js'
       }
+    },
+    cssmin: {
+        css: {
+            src: 'css/home.css',
+            dest: 'css/home.min.css'
+        }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['cssmin']);
 
 };
